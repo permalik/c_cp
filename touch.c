badd0801db3a195d396071d9f0ada9b94551d17f
permalik <permalik@protonmail.com>
2024-08-07 08:36:08 -0500

    feat accept input for touch

--- touch.c
+++ touch.c
@@ -1,31 +1,43 @@
-#include <errno.h>
+// #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 
-int main() {
-    int fd;
-    const char *filepath = "./test.txt";
+void create_file(char *file) {
+    char *response = NULL;
+    size_t len;
 
-    fd = open(filepath, O_CREAT | O_WRONLY, 0644);
-    if (fd == -1) {
-        perror("open");
-        printf("Error code: %d\n", errno);
-        return 1;
-    }
+    printf("%s", file);
+}
 
-    const char *text = "Hello Earl\n";
-    if (write(fd, text, 11) == -1) {
-        perror("write");
-        close(fd);
-        return 1;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("input filename");
+    } else {
+        create_file(argv[1]);
     }
+    // int fd;
+    // const char *filepath = "./test.txt";
 
-    if (close(fd) == -1) {
-        perror("close");
-        printf("Error code: %d\n", errno);
-        return 1;
-    }
+    // fd = open(filepath, O_CREAT | O_WRONLY, 0644);
+    // if (fd == -1) {
+    //     perror("open");
+    //     printf("Error code: %d\n", errno);
+    //     return 1;
+    // }
+
+    // const char *text = "Hello Earl\n";
+    // if (write(fd, text, 11) == -1) {
+    //     perror("write");
+    //     close(fd);
+    //     return 1;
+    // }
+
+    // if (close(fd) == -1) {
+    //     perror("close");
+    //     printf("Error code: %d\n", errno);
+    //     return 1;
+    // }
 
     return 0;
 }
